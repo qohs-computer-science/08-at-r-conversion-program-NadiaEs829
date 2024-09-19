8ca5e710930c57195b6f484b9c5d4448724b5994
NadiaEs829 <nadia_amores@hotmail.com>
2024-09-19 17:39:19 +0000

    working - end of 9/19

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -5,7 +5,7 @@
  * TO DO: Program Description: Decimal and Binary conversions
  */
 
-import java.util.Scanner;
+import java.util.Scanner;//to get user input
 public class HelloWorld {
 
 	public static void main(String[] args) {
@@ -14,12 +14,12 @@ public class HelloWorld {
 		System.out.println("Welcome! You have the following options");
 		System.out.println("1. Decimal to Binary");
 		System.out.println("2. Binary to Decimal");
-		System.out.println("Which type of conversion would would you like to preform?");
+		System.out.println("Which type of conversion would you like to preform?");
 		int option = input.nextInt();
 
 		while(option != 1 && option != 2){
 			System.out.println("You have two conversion options. Which one do you want to do?");
-			int option1 = input.nextInt();
+			option = input.nextInt();
 		}//end while
 
 		if(option == 1){
@@ -29,26 +29,28 @@ public class HelloWorld {
 
 		if(option == 2){
 			System.out.println("What is the binary number?");
-			string biNum = input.nextLine();
+			String biNum = input.nextLine();
 		}//end if
 
-			BinaryToDeci(biNum);
+			//BinaryToDeci(biNum);
 		//next step is to make 2 classes for binary and decimal
 		//after *return* the number
 
-		public static void BinaryToDeci(string num){
-			int ans = 0;
-			int pow = 0;
-			for(int i = num.length(); i >= 0; i--){
-				if(num.charAt(i) == "1")
-					ans += Math.pow(2,p);
-				p++;
-			}//end for loop
-			System.out.println("Decimal equivialent of " + num + " is " + ans);
-		}//end method
+		//public int DeciToBinary(int num){
 		
-		public static int DeciToBinary(int num){
-			
-		}//end method
+		//BinaryToDeci(biNum);
+
+		//}//end method
 	}//end main
+
+	public static String BinaryToDeci(String num){
+		int ans = 0;
+		int pow = 0;
+		for(int i = num.length(); i >= 0; i--){
+			if(num.charAt(i) == "1")
+				ans += Math.pow(2,pow);
+			pow++;
+		}//end for loop
+		System.out.println("Decimal equivialent of " + num + " is " + ans);
+	}//end method
 }//end class}
